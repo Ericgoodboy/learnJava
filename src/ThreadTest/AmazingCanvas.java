e29a840259e2c4e277dedc12b9841c7493ae18ab
Ericgoodboy <776938336@qq.com>
2019-05-24 19:29:26 +0800

    test2

--- src/ThreadTest/AmazingCanvas.java
+++ src/ThreadTest/AmazingCanvas.java
@@ -43,61 +43,17 @@ public class AmazingCanvas extends Canvas {
         timeString = "time:"+time;
     }
     private boolean isFirst = true;
-    public void paint(Graphics g){
-        if (isFirst){
-            paintHeader(g);
-            isFirst = true;
-        }
-        printTime(g);
-        if(mode==-1){
-            paintArray(g);
-        }else {
-            ThreadInfo threadInfo = map.get(mode);
-            paintItem(g,threadInfo.yPlace,threadInfo.process,threadInfo.name);
-        }
-    }
-    private void printTime(Graphics g){
-        g.clearRect(0,0,canvasWidth,headingTop);
-        g.setColor(Color.red);
-        g.drawString(timeString,260,10);
-    }
-    private void paintHeader(Graphics g){
-        g.setColor(Color.red);
-        g.drawLine(0,headingTop,canvasWidth,headingTop);
-        g.drawString(titleString,200,10);
-    }
+
     public void setProcess(int threadId,double process){
         map.get(threadId).runOnce(process);
         mode=threadId;
         repaint();
     }
-    public void paintArray(Graphics g){
-        int mapLength = map.size()+1;
-        Set<Integer> keys = map.keySet();
-        int skip = (canvesHeight-headingTop-boxHeight-30)/mapLength;
-        g.clearRect(0,headingTop+1,canvasWidth,canvesHeight);
-        int yPlace=skip+headingTop;
-        for(int key:keys){
-            ThreadInfo threadInfo = map.get(key);
-            threadInfo.yPlace = yPlace;
-            paintItem(g,yPlace,threadInfo.process,threadInfo.name);
-            yPlace+=skip;
-        }
-    }
+
     public void update(Graphics g){
         paint(g);
     }
-    private void paintItem(Graphics g,int yPlace,double processing,String name){
-        int xPlace = (int)((canvasWidth-boxWidth)*processing);
-        g.clearRect(0,yPlace-(int)(boxHeight/2),canvasWidth,boxHeight+1);
-        g.setColor(Color.red);
-        g.drawLine(0,yPlace,xPlace,yPlace);
-        int demox[]=new int[]{xPlace,xPlace,xPlace+boxWidth,xPlace};
-        int demoy[]=new int[]{yPlace-(int)(boxHeight/2),yPlace+(int)(boxHeight/2),yPlace,yPlace-(int)(boxHeight/2)};
-        g.drawPolygon(demox,demoy,demox.length);
-        g.setColor(Color.BLACK);
-        g.drawString(name,xPlace+3,yPlace+5);
-    }
+
     public void removeThread(int threadId){
         map.remove(threadId);
         synchronized (this){
@@ -113,4 +69,5 @@ public class AmazingCanvas extends Canvas {
             this.repaint();
         }
     }
+
 }
